9bc0470546df80ad3a5257610f12f138ee57e268
Vipul-Sahani <vipul.sahani@ics-global.in>
2025-03-06 10:59:21 +0530

    Delete RentalService directory

--- RentalService/src/main/java/com/example/RentalService/repo/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.RentalService.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.RentalService.model.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
